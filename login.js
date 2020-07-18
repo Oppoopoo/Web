a5865651609730a3e018c9b9c70654825b790c88
oppooName <jmy813@naver.com>
2020-07-18 16:23:18 +0900

    Tidy up..

---
+++ login.js
@@ -0,0 +1,12 @@
+function login(){
+    var id = document.getElementById("username").value;
+    var pw = document.getElementById("password").value;
+    if(id == ""){
+        alert("Please Input ID First.");
+    }
+    else if(pw == ""){
+        alert("Please Input Password First.");
+    }
+    //alert(id);
+    //console.log("Hello~~~");
+}
