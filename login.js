a47890ee65536edb6b580c38d30c94a9750652b3
oppooName <jmy813@naver.com>
2020-07-29 21:35:01 +0900

    Login..

--- login.js
+++ login.js
@@ -10,3 +10,27 @@ function login(){
     //alert(id);
     //console.log("Hello~~~");
 }
+
+function submitForm(formEl) {
+//아이디 / 패스워드  체크는 required해줌으로써 끝냄.
+    //var errorMessage = null;
+    //var objFocus = null;
+
+    // if (formEl.loginId.value.length == 0) {
+    //     errorMessage = "아이디를 넣어주세요.";
+    //     objFocus = formEl.loginId;
+    // }
+    // else if (formEl.loginPassword.value.length == 0) {
+    //     errorMessage = "비밀번호를 넣어주세요.";
+    //     objFocus = formEl.loginPassword;
+    // }
+    // if(errorMessage != null) {
+    //     alert(errorMessage);
+    //     objFocus.focus();
+    //     return false;
+    // }
+    //return true;
+    //location.href = "./main.html";
+    return true;
+    
+}
